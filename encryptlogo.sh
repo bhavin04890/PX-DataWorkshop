03982286b16081b154e70cdd3a74740f9faf8055
bhavin04890 <bshah@purestorage.com>
2022-10-14 10:00:57 -0400

    updated encrypt logo

--- encryptlogo.sh
+++ encryptlogo.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
-kubectl delete -f k8s-logo.yaml -n demo
+kubectl delete -f dataprotection/k8s-logo.yaml -n demo
 sleep 20
-kubectl delete -f postgres.yaml -n demo
+kubectl delete -f dataprotection/postgres.yaml -n demo
 sleep 10
 echo "Your Amazon EKS cluster has been under attack! All your applications have been encrypted!"
